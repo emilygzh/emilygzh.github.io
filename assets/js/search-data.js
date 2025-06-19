4af9cecc102caf7fdbed5d40b396ceb079d39245
emilygzh <62486203+emilygzh@users.noreply.github.com>
2025-06-19 22:48:40 +0000

    Deploying to gh-pages from @ emilygzh/emilygzh.github.io@57eb091390baf18945ddfd9665acb2699dce6698 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-blog",
           title: "blog",
           description: "",
